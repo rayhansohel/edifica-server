660d0301462f06883c3fddf9df57acfb06544ac2
RayhanSohel <arayhansohel@gmail.com>
2025-01-27 15:09:15 +0600

    implement Jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -33,60 +34,100 @@ async function run() {
     const apartmentCollection = client.db("edifica").collection("apartments");
     const agreementCollection = client.db("edifica").collection("agreements");
 
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // middleware verifyToken
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    //Middleware to verify admin after token verification
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    //Make User as an admin
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+
     // Fetch all user
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.json(users);
     });
 
     // Delete User
-    app.delete("/users/:id", async (req, res) => {
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
     });
 
-    //Make User as an admin
-    app.patch("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await userCollection.updateOne(filter, updatedDoc);
-      res.send(result);
-    });
-
     // change user role
-    app.patch("/users/role/:id", async (req, res) => {
+    app.patch("/users/role/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const { role } = req.body;
-    console.log(`${role}`)
       if (!role) {
         return res.status(400).json({ message: "Role is required" });
       }
-    
+
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: { role },
       };
-    
+
       try {
         const result = await userCollection.updateOne(filter, updatedDoc);
         if (result.modifiedCount > 0) {
           res.json({ message: "User role updated successfully" });
         } else {
-          res.status(404).json({ message: "User not found or no changes made" });
+          res
+            .status(404)
+            .json({ message: "User not found or no changes made" });
         }
       } catch (error) {
         console.error("Error updating user role:", error);
         res.status(500).json({ message: "Failed to update user role" });
       }
     });
-    
 
     // Fetch all apartments
     app.get("/all-apartments", async (req, res) => {
